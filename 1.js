39336886ec2e401226b223a0897abda708842aaf
saiamarnath9524 <sreeramasaiamarnath@gmail.com>
2025-01-08 04:27:26 +0530

    Revert "lab-2"
    This reverts commit 9c90c59cb9ee2e314154da9289e6e5277671fee6.

--- 1.js
+++
@@ -1,8 +0,0 @@
-//Write a function mergeArrays that accepts any number of arrays as arguments and merges them into a single array. Use the spread operator to accomplish this
-
-function mergeArrays(...arrays) {
-    return [...arrays.flat()]
-}
-
-let array1 = [[1, 2],[3,4],[5,6]]
-console.log(mergeArrays(array1))
--- 2.js
+++
@@ -1,8 +0,0 @@
-//Create a function calculateTotal that takes an arbitrary number of numeric values(using the rest operator) and returns their sum.
-
-function calculateTotal(...args)
-{
-    return args.reduce((a, b) => a + b, 0);
-}
-let arr=[10,20,30,40]
-console.log(calculateTotal(arr))
